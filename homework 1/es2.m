6d12b6bbf744745401f74e5870eae6b88143f19e
caradave <davidecaramagna5@gmail.com>
2023-12-06 22:16:48 +0100

    final stage

--- homework 1/es2.m
+++ homework 1/es2.m
@@ -6,4 +6,4 @@ a = [1, 2, 1];
 b = [3, 2, 1];
 
 h = conv(a, b);
-figure, stem([0: 4], h)
+figure, stem([0: 4], h), grid on
--- homework 1/es3.m
+++ homework 1/es3.m
@@ -6,41 +6,41 @@ clear all;
 % il primo segnale ha energia finita e potenza uguale a zero
 % calcolo dei primo 100 valori, utilizzati come campione, per calcolare
 % l'energia
-energia_1 = 0; 
-for i = 0:100
-    energia_1 = energia_1 + exp(-i);
+val_1 = []; 
+for i = 0:41
+    val_1 = horzcat(val_1, exp(-i));
 end
-energia_1
+energia_1 = energyCalculation(val_1)
 
 % il segnale ha energia infinita e potenza finita
-potenza_2 = powerCalculation(3*ones(1, 40))
+potenza_2 = powerCalculation(3*ones(1, 40), 40)
 
 % il terzo segnale ha energia infinita e potenza finita
 % l'esponenziale converge a uno, quindi rimane A
-potenza_3 = powerCalculation(3*ones(1, 40))
+potenza_3 = powerCalculation(3*ones(1, 40), 40)
 
 
 % il quarto segnale ha energia infinita e potenza  finita
-potenza_4 = powerCalculation(3*ones(1, 40))
+potenza_4 = powerCalculation(3*ones(1, 20), 40)
 
 % il quinto segnale ha energia finita e potenza uguale a zero
-energia_5 = energyCalculation(3*ones(1, 20))
+energia_5 = energyCalculation(3*ones(1, 5))
 
 
 %% implementazione del calcolo dell'energia e della potenza
 function energy = energyCalculation(vett)
     energy = 0;
     for i = 1:length(vett)
-        energy = energy + vett(i)^2;
+        energy = energy + abs(vett(i))^2;
     end
 end
 
-function power = powerCalculation(vett)
+function power = powerCalculation(vett, dim)
     power = 0;
     for i = 1:length(vett)
         power = power + vett(i)^2;
     end
     
-    power = power/(length(vett)+1);
+    power = power/(dim);
 
 end
